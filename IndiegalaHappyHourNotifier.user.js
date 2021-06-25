f36749abf3e01ee37a898db7ee1f1b651836cac1
Sergei <ZeroUnderscoreOu@users.noreply.github.com>
2021-06-25 07:35:48 +0300

    Updated to work with current version of IG site

--- IndiegalaHappyHourNotifier.user.js
+++ IndiegalaHappyHourNotifier.user.js
@@ -1,34 +1,44 @@
 // ==UserScript==
 // @name        Indiegala Happy Hour Notifier
 // @author      ZeroUnderscoreOu
-// @version     1.0.0
+// @version     2.0.0
 // @icon        
 // @description Simple checker for any Indiegala bundles being on Happy Hour
 // @namespace   https://github.com/ZeroUnderscoreOu/
-// @match       *://www.indiegala.com/
+// @match       *://www.indiegala.com/bundle/*
 // @run-at      document-idle
 // @grant       none
 // ==/UserScript==
 
-Array.from(document.querySelectorAll("Div.homebundle-type")).forEach((Div)=>{
+if (!localStorage.getItem("IGHHNInit")) { // one-time initialization
+	Notification.requestPermission();
+	localStorage.setItem("IGHHNSeen",["List of already seen bundles"]);
+	localStorage.setItem("IGHHNInit",true);
+};
+
+Array.from(document.getElementsByClassName("bundle-page-other-bundles-extra-info")).forEach((Div)=>{
 	if (Div.textContent.includes("happy hour")) {
 		Div = Div.parentElement;
-		let Name = Div.querySelector("Img.imgbig").alt;
-		let Check = localStorage.getItem(Name); // checking if bundle was already opened
-		if (!Check) {
-			let Time = Div.querySelector("Span.timeend").textContent;
-			Notification.requestPermission().then(()=>{
-				new Notification("Indiegala Happy Hour",{
-					icon: "https://www.indiegala.com/favicon.ico",
-					body: `Happy hour is on for ${Name}\r\n${Time} left`,
-					requireInteraction: true
-				});
+		let A = Div.firstElementChild;
+		let Name = A.title;
+		let Check = localStorage.getItem("IGHHNSeen").split(",");
+		if (!Check.includes(Name)) { // checking if bundle was already opened
+			let N = new Notification("Indiegala Happy Hour",{
+				icon: "https://www.indiegala.com/favicon.ico",
+				body: `Happy hour is on for ${Name}`,
+				data: A.href,
+				requireInteraction: true,
+				renotify: true
 			});
-			localStorage.setItem(Name,Date.now()); // storing the bundle to not open again
-			window.location.href = Div.querySelector("A.bundle-link").href; // opening the bundle
+			N.addEventListener("click",(Data)=>{ // setting the action to open bundle page
+				window.open(A.href,"_blank");
+			});
+			Check.push(Name);
+			localStorage.setItem("IGHHNSeen",Check); // storing the bundle to not open again
 		};
 	};
 });
+
 setTimeout(()=>{ // if nothing is opened
 	window.location.reload(); // reloading; can't call reload() directly for some reason
 },30*60*1000); // 30 mins
